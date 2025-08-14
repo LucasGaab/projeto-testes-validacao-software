486763aeb44019cfdb9efe43a08a69d8b54f1ca6
Allyson Ryan <115114528+AllysonRyanE@users.noreply.github.com>
2025-08-14 17:01:06 -0300

    Update alluses.test.js

--- programas-alvo/math-utils/test/alluses.test.js
+++ programas-alvo/math-utils/test/alluses.test.js
@@ -90,3 +90,4 @@ describe('All-Uses Coverage Suite (Corrigida)', () => {
     // O esperado é 0, mas a função com bug retornará um número pequeno e negativo.
     expect(avaliarExpressao('0.2 - 0.3')).toBe(0);
 });
+});
