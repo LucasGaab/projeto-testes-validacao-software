0222f120fdf806de2795c8125739d786011904d6
Allyson Ryan <115114528+AllysonRyanE@users.noreply.github.com>
2025-08-14 17:01:19 -0300

    Update branch.test.js

--- programas-alvo/math-utils/test/branch.test.js
+++ programas-alvo/math-utils/test/branch.test.js
@@ -93,3 +93,4 @@ describe('Branch Coverage Suite (Corrigida)', () => {
     // O esperado é 0, mas a função com bug retornará um número pequeno e negativo.
     expect(avaliarExpressao('0.2 - 0.3')).toBe(0);
 });
+});
